56842088f092501a3193e2ded8ae76241b2dd86f
Beltrán José Aceves Gil <beltran.aceves@udc.es>
2022-01-14 13:43:43 +0100

    Change example to milestone2

--- project_v2/Milestone2/example.js
+++ project_v2/Milestone2/example.js
@@ -1,12 +1,10 @@
 function sliceMe() {
-    var x;
-    var y;
-    var z;
-    x = 1;
-    y = 2;
-    z = x + y;
-    console.log("patata");
-    return x;
+  var x;
+  var y;
+  x = 1;
+  y = 2;
+  x = x + y;
+  y += 2;
+  return y; // slicing criterion
 }
-
 sliceMe();
