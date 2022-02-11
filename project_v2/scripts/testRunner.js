edeee400c5144f547701935b0fb84c02ea59f73c
beltranaceves <beltranaceves@gmail.com>
2022-02-11 03:26:33 +0100

    Updated testRunner

--- project_v2/scripts/testRunner.js
+++ project_v2/scripts/testRunner.js
@@ -1,5 +1,7 @@
 (function() {
 	const fs = require('fs');
+	const escodegen = require('escodegen');
+    const { Parser } = require('acorn');
 	var levenshtein = require('fast-levenshtein');
 	const { ArgumentParser } = require("argparse");
 	const parser = new ArgumentParser({
@@ -11,12 +13,20 @@
     const args = parser.parse_args();
 	
 	function readFile(fileName){
-		return fs.readFileSync(fileName, 'utf8');	
+		return fs.readFileSync(fileName, 'utf8');
+	}
+
+	function reformatTestCode(codeString){
+		const program = Parser.parse(codeString,
+            { ecmaVersion: 5, locations: true }
+        )
+		const program_string = escodegen.generate(program)
+		return program_string
 	}
 
 	function compare(originalFile, predictedFile){
-		expectedSlice = readFile(originalFile);
-		predictedSlice = readFile(predictedFile);
+		expectedSlice  = reformatTestCode(readFile(originalFile));
+		predictedSlice = reformatTestCode(readFile(predictedFile));
 
 		if (expectedSlice === predictedSlice){
 			console.log("exact match");
@@ -38,12 +48,12 @@
 		// create input parameters from args ditcionary
 		inputArgs = " --inFile "+element["inFile"]+" --outFile "+element["outFile"]+" --lineNb "+element["lineNb"];
 		stmt = 'node slice.js' + inputArgs;
-					
+				
 		var exec = require('child_process').exec,
 		    child;
 		
 		child = exec(stmt,
-		  function (error, stdout, stderr) { // Mantiene 
+		  function (error, stdout, stderr) {
 		    console.log('stdout: ' + stdout); // status message after executing slice.js
 		    compare(element["goldFile"],element["outFile"]); // compare method to evaluate expected and predicted slice
 		    if (error !== null) {
